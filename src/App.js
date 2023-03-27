f118ff0c727b20fd2fda986b18e5ff2d140e818a
PkSmart <p1ooooo100@gmail.com>
2023-03-27 09:58:05 +0530

    creating navbar as well as components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import Home from "./screens/Home";
 
 
 function App() {
   return (
-  <div className ="fs-1">
-    start
-  </div>
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
   );
 }
 
