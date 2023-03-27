32b7fbd586eec79efe6b90eadd4046a3302ea6df
PkSmart <p1ooooo100@gmail.com>
2023-03-27 12:51:49 +0530

    setting up navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Home from "./screens/Home";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
 
 function App() {
   return (
-    <>
+    <Router>
       <div>
-        <Home/>
+        <Routes>
+          <Route exact path = '/' element ={ <Home/> }></Route>
+          <Route exact path = '/login' element ={ <Login/> }></Route>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
