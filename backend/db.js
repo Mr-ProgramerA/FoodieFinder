1d8a1b8a807e381f6cebd8b297aba4f4f44fcddc
PkSmart <p1ooooo100@gmail.com>
2023-04-24 19:55:42 +0530

    mongoose error solved !!! downgraded mongoose version to 6.10.0 from 7+ data read form atlas successful

--- backend/db.js
+++ backend/db.js
@@ -9,63 +9,72 @@ const mongoURI =
 //   "mongodb://GoFood:GoFoodProjectPassword@ac-k2ycmwo-shard-00-00.uw6idog.mongodb.net:27017,ac-k2ycmwo-shard-00-01.uw6idog.mongodb.net:27017,ac-k2ycmwo-shard-00-02.uw6idog.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-tyuld5-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const mongoDB = async () => {
-  await mongoose
-    .connect(mongoURI,{
-      useNewUrlParser: true,
+
+  mongoose.connect(mongoURI,{useNewUrlParser:true}, async(err,result) => {
+    if (err) console.log(err);
+    else {
+      console.log("connected");
+      const fetched_data = await mongoose.connection.db.collection("food_items")
+      fetched_data.find({}).toArray(function (err,data) {
+        if (err) console.log(err);
+        else console.log(data);
+      })
+    }
+  })
+  }
+  //   ########################################################################
+  
+
+  // await mongoose
+  //   .connect(mongoURI)
+      // useNewUrlParser: true,
       // useCreateIndex: true,
       // useUnifiedTopology: true,
       // useFindAndModify: false 
-    })
-    .then(() => {
-      console.log("success!!!");
-      const fetched_data = mongoose.connection.db.collection("food_items");
-      console.log(fetched_data);
-      fetched_data.find({}).toArray(function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    // })
+    // .then(() => {
+    //   console.log("success!!!");
+    //   const fetched_data = mongoose.connection.db.collection("food_items");
+    //   console.log(fetched_data);
+    //   fetched_data.find({}).toArray(function (err,data) {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log(data);
+    //     }
+    //   })
+    // })
+      
+      
+    //   .then(function (data) {
+    //     console.log(data);
+    //   }).catch(function (err) {
+    //     console.log(err);
+    //   })
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
 
   //   ########################################################################
 
-  // await mongoose.connect(mongoURI);
-  // console.log("success!!!");
-  // const fetched_data = mongoose.connection.db.collection("food_items");
-  // console.log(fetched_data);
-  // fetched_data.find({}).toArray(function (err, data) {
-  //   if (err) console.log(err);
-  //   else console.log(data);
-  // });
-
-  // fetched_data.find({}).toArray(function(err,data){
-  //   if (err) console.log(err);
-  //   else console.log(data);
-  // })
-};
-
-//   ########################################################################
 
 //   mongoose.connect(mongoURI, () => {
 // console.log("connected");
 //   });
-
+// }
 //   ########################################################################
 
-//   await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
-//     if (err) console.log("--------", err);
-//     else {
-//       console.log("connected");
-//     }
-//   });
+  // await mongoose.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+  //   if (err) console.log("--------", err);
+  //   else {
+  //     console.log("connected");
 
+  //   }
+  // });
+
+  // };
 // #############################################################################
 
-// };
 
 module.exports = mongoDB;
