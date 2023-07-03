88b547785424ab070a7a2fe7da44108eab1ca191
PkSmart <p1ooooo100@gmail.com>
2023-07-03 20:54:39 +0530

    some changes in logo, setting signup page and making submit handlers, method and usestate is to be build

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar"
+import { Link } from "react-router-dom";
+
+const handleSubmit = async(e) => {
+  e.preventDefault()
+  const response = fetch('http://localhost:5000/api/createuser',{
+    
+  })
+}
 
 function Signup() {
   return (
     <>
     <div> <Navbar/> </div>
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}s>
           <div className="mb-3">
             <label for="name" className="form-label">
               Name
             </label>
             <input type="text" className="form-control" />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
           </div>
           <div className="mb-3">
             <label for="exampleInputEmail1" className="form-label">
@@ -41,9 +46,10 @@ function Signup() {
             />
           </div>
           
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn m-3 btn-success">
             Submit
           </button>
+          <Link className="btn m-3 btn-warning" to = "/login">Already a user !</Link>
         </form>
       </div>
     </>
