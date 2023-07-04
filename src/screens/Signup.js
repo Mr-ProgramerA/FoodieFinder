733fbaf09db939083adbc142765661574ad0edc7
PkSmart <p1ooooo100@gmail.com>
2023-07-04 20:32:19 +0530

    form onChange function and useState made. form submission api is to be mage

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,25 +1,48 @@
-import React from "react";
-import Navbar from "../components/Navbar"
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
-const handleSubmit = async(e) => {
-  e.preventDefault()
-  const response = fetch('http://localhost:5000/api/createuser',{
-    
-  })
-}
-
 function Signup() {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    geolocation: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = fetch("http://localhost:5000/api/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    });
+  };
+
+  const onChangeForm = (event) => {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
   return (
     <>
-    <div> <Navbar/> </div>
+      <div>
+        {" "}
+        <Navbar />{" "}
+      </div>
       <div className="container">
-        <form onSubmit={handleSubmit}s>
+        <form onSubmit={handleSubmit} s>
           <div className="mb-3">
             <label for="name" className="form-label">
               Name
             </label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              name="name"
+              value={credentials.name}
+              onChange={onChangeForm}
+              className="form-control"
+            />
           </div>
           <div className="mb-3">
             <label for="exampleInputEmail1" className="form-label">
@@ -30,6 +53,9 @@ function Signup() {
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
+              name="email"
+              value={credentials.email}
+              onChange={onChangeForm}
             />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
@@ -43,13 +69,29 @@ function Signup() {
               type="password"
               className="form-control"
               id="exampleInputPassword1"
+              name="password"
+              value={credentials.password}
+              onChange={onChangeForm}
+            />
+            <label for="geolocation" className="form-label mt-3">
+              Addresss
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="geolocation"
+              name="geolocation"
+              value={credentials.geolocation}
+              onChange={onChangeForm}
             />
           </div>
-          
+
           <button type="submit" className="btn m-3 btn-success">
             Submit
           </button>
-          <Link className="btn m-3 btn-warning" to = "/login">Already a user !</Link>
+          <Link className="btn m-3 btn-warning" to="/login">
+            Already a user !
+          </Link>
         </form>
       </div>
     </>
