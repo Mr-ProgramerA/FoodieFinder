34289b77fd93fdd9ab7b4f26a78b4e83c2651e7f
PkSmart <p1ooooo100@gmail.com>
2023-07-15 22:17:44 +0530

    jwt token set

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+// random string
+const jwtSecret = "aB3@Kt87cD#vLn!w&y5zXqP*2FsN$r@G";
 
 router.post(
   "/createuser",
@@ -58,11 +60,23 @@ router.post(
           .json({ errors: "try logging with correct credentials" });
       }
 
-      const pwdCompare = await bcrypt.compare(req.body.password,userData.password)
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
       if (!pwdCompare) {
         return res.status(400).json({ errors: "Wrong password" });
       }
-      return res.json({ success: true });
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      return res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
