d819fed96b90b07071dc65deae8ad8b7847f3804
PkSmart <p1ooooo100@gmail.com>
2023-07-26 21:28:49 +0530

    working in card component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json()
+
+    setFoodItem(response[0])
+    setFoodCat(response[1])
+  };
+
+  useEffect(() => {
+    loadData()
+  },[])
+  
+
   return (
     <div>
       <div>
@@ -13,9 +34,16 @@ function Home() {
       <div>
         <Carousel />
       </div>
-      <div className="m-3">
-        <Card />
-        <Card />
+      <div className="container">
+      {
+        foodCat !== []
+        ? foodCat.map((data)=> {
+          return(
+            <div>a card item</div>
+          )
+        })
+        : <div>couldn't load data</div>
+      }
         <Card />
       </div>
 
