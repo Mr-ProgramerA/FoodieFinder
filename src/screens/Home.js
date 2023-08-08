60bea93f29a71361d7db3ab235ca0075cec74e56
PkSmart <p1ooooo100@gmail.com>
2023-08-08 18:48:48 +0530

    search feature working in carousel carousel code copied and pasted inside Home.js (due to search feature is requires data from filter() )

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
-import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 function Home() {
+  const [search, setSearch] = useState("");
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
@@ -31,7 +31,97 @@ function Home() {
         <Navbar />
       </div>
       <div>
-        <Carousel />
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          data-interval="1000"
+        >
+          <div className="carousel-inner">
+            <div
+              style={{ zIndex: 10 }}
+              className="carousel-caption d-none d-md-block"
+            >
+              <div className="d-flex justifiy-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                {/* <button className="btn btn-outline-warning" type="submit">
+                Search
+              </button> */}
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/860x300?lunch"
+                className="d-block w-100"
+                alt="carousal image"
+                style={{ filter: "brightness(32%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/860x300?dinner"
+                className="d-block w-100"
+                alt="carousal image"
+                style={{ filter: "brightness(32%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/860x300?seafood"
+                className="d-block w-100"
+                alt="carousal image"
+                style={{ filter: "brightness(32%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/860x300?vegetarianfood"
+                className="d-block w-100"
+                alt="carousal image"
+                style={{ filter: "brightness(32%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/860x300?juice"
+                className="d-block w-100"
+                alt="carousal image"
+                style={{ filter: "brightness(32%)" }}
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
       <div className="container">
         {foodCat !== [] ? (
@@ -44,7 +134,11 @@ function Home() {
                 <hr />
                 {foodItem !== [] ? (
                   foodItem
-                    .filter((item) => item.CategoryName == data.CategoryName)
+                    .filter(
+                      (item) =>
+                        item.CategoryName == data.CategoryName &&
+                        item.name.toLowerCase().includes(search.toLowerCase())
+                    )
                     .map((filterItems) => {
                       return (
                         <div
@@ -55,7 +149,7 @@ function Home() {
                             foodName={filterItems.name}
                             options={filterItems.options[0]}
                             imgSrc={filterItems.img}
-                            desc = {filterItems.description}
+                            desc={filterItems.description}
                           />
                         </div>
                       );
